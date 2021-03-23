ce43f224cde38703ecbe04c8b3be0a02dd2a34b1
Amy While <26681721+elihwyma@users.noreply.github.com>
2021-03-23 19:09:55 +0000

    Image optimising, love you imgbot

---
+++ Sileo/Backend/Canister Resolver/CanisterResolver.swift
@@ -0,0 +1,15 @@
+//
+//  CanisterResolver.swift
+//  Sileo
+//
+//  Created by Amy on 23/03/2021.
+//  Copyright © 2021 CoolStar. All rights reserved.
+//
+
+import Foundation
+
+final class CanisterResolver {
+    
+    public static let shared = CanisterResolver()
+    
+}
