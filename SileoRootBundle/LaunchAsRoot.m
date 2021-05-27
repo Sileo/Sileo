a48b1ceedc17b2af4a85a235d5386775ecc6162e
Amy While <26681721+elihwyma@users.noreply.github.com>
2021-05-27 18:50:33 +0100

    sorry capt

--- SileoRootBundle/LaunchAsRoot.m
+++ SileoRootBundle/LaunchAsRoot.m
@@ -9,43 +9,6 @@
 #import "LaunchAsRoot.h"
 #pragma clang diagnostic ignored "-Wdeprecated-declarations"
 
-/*
- @implementation LaunchAsRoot
- -(void)launchAsRoot {
-     if (geteuid()) {
-         OSStatus status;
-         AuthorizationFlags flags = kAuthorizationFlagDefaults;
-         AuthorizationRef authRef;
-         
-         status = AuthorizationCreate(NULL, kAuthorizationEmptyEnvironment, flags, &authRef);
-         if (status != errAuthorizationSuccess) exit(0);
-         
-         AuthorizationItem items = {kAuthorizationRightExecute, 0, NULL, 0};
-         AuthorizationRights rights = {1, &items};
-         flags = kAuthorizationFlagDefaults |
-         kAuthorizationFlagInteractionAllowed |
-         kAuthorizationFlagPreAuthorize |
-         kAuthorizationFlagExtendRights;
-         status = AuthorizationCopyRights(authRef, &rights, NULL, flags, NULL );
-         
-         
-         if (status != errAuthorizationSuccess) {
-             exit(0);
-         }
-         
-         const char * path = [[[NSBundle mainBundle] executablePath] UTF8String];
-         char * args[] = {NULL};
-         
-         flags = kAuthorizationFlagDefaults;
-         status = AuthorizationExecuteWithPrivileges(authRef, path, flags, args,
-                                                       NULL);
-         AuthorizationFree(authRef, kAuthorizationFlagDefaults);
-         exit(0);
-     }
- }
- @end
- */
-
 @implementation LaunchAsRoot
 
 AuthorizationRef authRef;
