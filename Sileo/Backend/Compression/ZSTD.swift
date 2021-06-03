350b1daff81bbeae2dc58c33425286afcd487ce1
Amy While <26681721+elihwyma@users.noreply.github.com>
2021-06-03 21:49:17 +0100

    Finally a fix for zstd crash?

--- Sileo/Backend/Compression/ZSTD.swift
+++ Sileo/Backend/Compression/ZSTD.swift
@@ -23,19 +23,14 @@ final class ZSTD {
         defer {
             fclose(fin)
         }
+
         AmyLogManager.log("[Sileo] Loading buffIn")
         let buffInSize = ZSTD_DStreamInSize()
-        AmyLogManager.log("[Sileo] malloc \(buffInSize)")
-        guard let buffIn = malloc(buffInSize) else { return (ZSTDError.buffIn.rawValue, nil) }
-        defer {
-            free(buffIn)
-        }
         AmyLogManager.log("[Sileo] Loading buffOut")
         let buffOutSize = ZSTD_DStreamOutSize()
-        guard let buffOut = malloc(buffOutSize) else { return (ZSTDError.buffOut.rawValue, nil) }
-        defer {
-            free(buffOut)
-        }
+        AmyLogManager.log("[Sileo] malloc \(buffOutSize)")
+        let inBuf = UnsafeMutablePointer<UInt8>.allocate(capacity: Int(buffInSize))
+        let outBuf = UnsafeMutablePointer<UInt8>.allocate(capacity: Int(buffOutSize))
         AmyLogManager.log("[Sileo] Loading ZSTD_createDCTX")
         guard let dctx = ZSTD_createDCtx() else { return (ZSTDError.context.rawValue, nil) }
         defer {
@@ -46,15 +41,15 @@ final class ZSTD {
         var isEmpty = true
         let data = NSMutableData()
         while true {
-            AmyLogManager.log("[Sileo] fread \(buffIn) \(1) \(buffInSize) \(fin)")
-            read = fread(buffIn, 1, buffInSize, fin)
+            AmyLogManager.log("[Sileo] fread \(inBuf) \(1) \(buffInSize) \(fin)")
+            read = fread(inBuf, 1, buffInSize, fin)
             if read == 0 { break }
             isEmpty = false
-            AmyLogManager.log("[Sileo] ZSTD_in_buffer \(buffIn) \(read) \(0)")
-            var input = ZSTD_inBuffer(src: buffIn, size: read, pos: 0)
+            AmyLogManager.log("[Sileo] ZSTD_in_buffer \(inBuf) \(read) \(0)")
+            var input = ZSTD_inBuffer(src: inBuf, size: read, pos: 0)
             while input.pos < input.size {
-                AmyLogManager.log("[Sileo] outBuffer \(buffOut) \(buffOutSize) \(0)")
-                var output = ZSTD_outBuffer(dst: buffOut, size: buffOutSize, pos: 0)
+                AmyLogManager.log("[Sileo] outBuffer \(outBuf) \(buffOutSize) \(0)")
+                var output = ZSTD_outBuffer(dst: outBuf, size: buffOutSize, pos: 0)
                 AmyLogManager.log("[Sileo] decompressStream \(dctx) \(output) \(input)")
                 let ret = ZSTD_decompressStream(dctx, &output, &input)
                 if ZSTD_isError(ret) != 0 {
@@ -65,8 +60,8 @@ final class ZSTD {
                         return (ZSTDError.unknown.rawValue, nil)
                     }
                 }
-                AmyLogManager.log("[Sileo] Data Append \(buffOut) \(output.pos)")
-                data.append(Data(bytes: buffOut, count: output.pos))
+                AmyLogManager.log("[Sileo] Data Append \(outBuf) \(output.pos)")
+                data.append(Data(bytes: outBuf, count: output.pos))
                 lastRet = ret
             }
         }
