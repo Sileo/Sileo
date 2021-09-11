db68476653eb20d95c5cb77eadabeff3f22b326e
Amy While <26681721+elihwyma@users.noreply.github.com>
2021-09-11 17:44:32 +0100

    Revert "trolling"
    This reverts commit 076174e26ef3846231ec88100593526f9b3ed2e9.

--- Sileo/Backend/Objects/Package.swift
+++ Sileo/Backend/Objects/Package.swift
@@ -31,9 +31,9 @@ final class Package: Hashable, Equatable {
     public var tags: PackageTags = .none
     public var nativeDepiction: String?
     
-    public var allVersionsInternal = [String: Package]()
+    public var allVersionsInternal = [String: PackageOld]()
     public var allVersions: [Package] {
-        var allVersionsInternal = Array(allVersionsInternal.values)
+        var allVersionsInternal = allVersionsInternal.map { $1.packageNew }
         allVersionsInternal.insert(self, at: 0)
         return allVersionsInternal
     }
@@ -76,17 +76,100 @@ final class Package: Hashable, Equatable {
     
     public func addOld(_ packages: [Package]) {
         for package in packages {
-            if package == self { continue }
+            let packageOld = PackageOld(package: package)
+            allVersionsInternal[packageOld.version] = packageOld
+        }
+    }
+    
+    public func addOldInternal(_ packages: [PackageOld]) {
+        for package in packages {
             allVersionsInternal[package.version] = package
         }
     }
     
     public func getVersion(_ version: String) -> Package? {
         if version == self.version { return self }
-        return allVersionsInternal[version]
+        if let package = allVersionsInternal[version] {
+            return package.packageNew
+        }
+        return nil
     }
 }
 
 func == (lhs: Package, rhs: Package) -> Bool {
+    lhs.package == rhs.package && lhs.version == rhs.version
+}
+
+final class PackageOld: Hashable, Equatable {
+    public var sourceFile: String?
+    public var package: String
+    public var packageID: String
+    public var name: String?
+    public var version: String
+    public var rawControl: [String: String] = [:]
+    public var rawData: Data?
+    public var sourceFileURL: URL?
+    public var source: String?
+    public var essential: String?
+    public var commercial: Bool = false
+    public var filename: String?
+    public var size: String?
+    public var packageFileURL: URL?
+    public var architecture: String?
+    public var installedSize: Int?
+    public var author: String?
+    public var maintainer: String?
+    public var nativeDepiction: String?
+    
+    init(package: Package) {
+        self.sourceFile = package.sourceFile
+        self.package = package.package
+        self.packageID = package.packageID
+        self.name = package.name
+        self.version = package.version
+        self.rawControl = package.rawControl
+        self.rawData = package.rawData
+        self.sourceFileURL = package.sourceFileURL
+        self.source = package.source
+        self.essential = package.essential
+        self.commercial = package.commercial
+        self.filename = package.filename
+        self.size = package.size
+        self.packageFileURL = package.packageFileURL
+        self.architecture = package.architecture
+        self.installedSize = package.installedSize
+        self.author = package.author
+        self.maintainer = package.maintainer
+        self.nativeDepiction = package.nativeDepiction
+    }
+    
+    public var packageNew: Package {
+        let package = Package(package: self.package, version: self.version)
+        package.sourceFile = self.sourceFile
+        package.name = self.name
+        package.rawControl = self.rawControl
+        package.rawData = self.rawData
+        package.sourceFileURL = self.sourceFileURL
+        package.source = self.source
+        package.essential = self.essential
+        package.commercial = self.commercial
+        package.filename = self.filename
+        package.size = self.size
+        package.packageFileURL = self.packageFileURL
+        package.architecture = self.architecture
+        package.installedSize = self.installedSize
+        package.maintainer = self.maintainer
+        package.author = self.author
+        package.nativeDepiction = self.nativeDepiction
+        return package
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(package)
+        hasher.combine(version)
+    }
+}
+
+func == (lhs: PackageOld, rhs: PackageOld) -> Bool {
     lhs.package == rhs.package && lhs.version == rhs.version
 }
