9a12d214da52c258d22d028370046e4693f9671d
Amy While <26681721+elihwyma@users.noreply.github.com>
2021-10-09 14:20:15 +0100

    Update Evander Version and remove old extension

--- Sileo/Backend/Package Manager/PackageListManager.swift
+++ Sileo/Backend/Package Manager/PackageListManager.swift
@@ -578,18 +578,3 @@ final class PackageListManager {
         }
     }
 }
-
-extension Thread {
-
-    var threadName: String {
-        if let currentOperationQueue = OperationQueue.current?.name {
-            return "OperationQueue: \(currentOperationQueue)"
-        } else if let underlyingDispatchQueue = OperationQueue.current?.underlyingQueue?.label {
-            return "DispatchQueue: \(underlyingDispatchQueue)"
-        } else {
-            let name = __dispatch_queue_get_label(nil)
-            return String(cString: name, encoding: .utf8) ?? Thread.current.description
-        }
-    }
-    
-}
