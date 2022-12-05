3024536255179bf490ba5528162d69c09d10c7fe
0xilis <109871561+0xilis@users.noreply.github.com>
2022-12-05 15:55:59 -0500

    update package list item

--- Sileo/UI/PackageListViewController/PackageCell/PackageCollectionViewCell.swift
+++ Sileo/UI/PackageListViewController/PackageCell/PackageCollectionViewCell.swift
@@ -42,6 +42,35 @@ class PackageCollectionViewCell: SwipeCollectionViewCell {
                 EvanderNetworking.image(url: url, condition: { [weak self] in self?.targetPackage?.icon == url }, imageView: imageView, fallback: targetPackage.defaultIcon)
                         
                 titleLabel?.textColor = targetPackage.commercial ? self.tintColor : .sileoLabel
+                
+                //#if os(iOS)
+
+                let deponfw = (targetPackage.depends ?? "Depends: firmware (>= 0.0)") + ","
+
+                if deponfw.contains("firmware") {
+                    if doesNotDepend(confOrDependString: deponfw, forVersion: Float(UIDevice.current.systemVersion)!) {
+                        titleLabel?.textColor = UIColor.red
+                        targetPackage.isFirmwareConflict = true
+                    } else {
+                        let conflictwithfw = (targetPackage.conflicts ?? "Conflicts: firmware (>= 9999999.0)") + ","
+                        if conflictwithfw.contains("firmware") {
+                            if !doesNotDepend(confOrDependString: conflictwithfw, forVersion: Float(UIDevice.current.systemVersion)!) {
+                                titleLabel?.textColor = UIColor.red
+                                targetPackage.isFirmwareConflict = true
+                            }
+                        }
+                    }
+                } else {
+                    let conflictwithfw = (targetPackage.conflicts ?? "Conflicts: firmware (>= 9999999.0)") + ","
+                    if conflictwithfw.contains("firmware") {
+                        if !doesNotDepend(confOrDependString: conflictwithfw, forVersion: Float(UIDevice.current.systemVersion)!) {
+                            titleLabel?.textColor = UIColor.red
+                            targetPackage.isFirmwareConflict = true
+                        }
+                    }
+                }
+
+                //#endif
             }
             unreadView?.isHidden = true
             
@@ -104,7 +133,7 @@ class PackageCollectionViewCell: SwipeCollectionViewCell {
     }
     
     @objc func updateSileoColors() {
-        if !(targetPackage?.commercial ?? false) {
+        if !(targetPackage?.commercial ?? false) && !(targetPackage?.isFirmwareConflict ?? false) {
             titleLabel?.textColor = .sileoLabel
         }
     }
@@ -132,8 +161,12 @@ class PackageCollectionViewCell: SwipeCollectionViewCell {
     override func tintColorDidChange() {
         super.tintColorDidChange()
         
-        if targetPackage?.commercial ?? false {
-            titleLabel?.textColor = self.tintColor
+        if targetPackage?.isFirmwareConflict ?? false {
+            titleLabel?.textColor = UIColor.red
+        } else {
+            if targetPackage?.commercial ?? false {
+                titleLabel?.textColor = self.tintColor
+            }
         }
         
         unreadView?.backgroundColor = self.tintColor
@@ -443,5 +476,76 @@ extension PackageCollectionViewCell: SwipeCollectionViewCellDelegate {
             let generator = UINotificationFeedbackGenerator()
             generator.notificationOccurred(.success)
         }
+    }
+    
+    //Thanks to Serena for original function, this is changed by me (Snoolie/0xilis) from the original to fix some bugs and make it slightly faster.
+    //This function is licensed under MIT at https://github.com/0xilis/SileoRedLabelFirmwareConflict
+    //It take in a Depends (or Pre-Depends) and will return true if the firmware is in regards
+    private func doesNotDepend(confOrDependString: String, forVersion ver: Float) -> Bool {
+        let noSpaceFull = confOrDependString.replacingOccurrences(of: " ", with: "");
+        var noSpaceIter = 0
+        for noSpacet in noSpaceFull.components(separatedBy: ",firmware")  { //support for two firmware depends in one field
+            var noSpace = ""
+            if noSpaceIter == 0 {
+                if !noSpaceFull.contains(":firmware") { //handle if :firmware
+                    noSpaceIter += 1
+                    continue;
+                } else {
+                    noSpace = noSpacet
+                }
+            } else {
+                noSpace = "firmware" + noSpacet
+            }
+            guard let startIndex = noSpace.range(of: "firmware")?.upperBound, let endIndex = noSpace.range(of: ",", options: .init(rawValue: 0), range: startIndex..<noSpace.endIndex, locale: nil)?.lowerBound else {
+                noSpaceIter += 1
+                continue
+            }
+            let baseString = String(noSpace[startIndex..<endIndex]).replacingOccurrences(of: ")", with: "") //we shouldn't need to remove (, only )
+            if baseString.contains("|") {
+                print("something other than firmware")
+                noSpaceIter += 1
+                continue //we don't actually want to mark as red if something other than the firmware conflicts, so firmware (>= 13.0) | somedeb gets marked as false even if firmware is lower than 13.0, since somedeb isn't firmware
+            }
+            var operatorType = 0
+            //operatorTypes:
+            //> / >> are 1
+            //< / << are 2
+            //= is 3
+            //>= is 4
+            //<= is 5
+            var iterator = 0
+            var versionParsed = Float(-1)
+            for i in baseString {
+                if i.isWholeNumber {
+                    versionParsed = Float(baseString.dropFirst(iterator)) ?? Float(-1)
+                    break; //we found the char
+                } else if i == ">" {
+                    operatorType = 1
+                } else if i == "<" {
+                    operatorType = 2
+                } else if i == "=" {
+                    operatorType += 3
+                }
+                iterator += 1
+            }
+            if versionParsed == -1 {
+                print("COULD NOT FIND VERSIONPARSED")
+                print(confOrDependString)
+                print(baseString)
+                print(noSpaceIter)
+                noSpaceIter += 1
+                continue
+            }
+            noSpaceIter += 1
+            switch operatorType {
+                case 1: if (versionParsed >= ver) {return versionParsed >= ver}
+                case 2: if (versionParsed <= ver) {return versionParsed <= ver}
+                case 4: if (versionParsed > ver) {return versionParsed > ver}
+                case 5: if (versionParsed < ver) {return versionParsed < ver}
+                case 3: if (versionParsed != ver) {return versionParsed != ver}
+                default: continue;
+            }
+        }
+        return false
     }
 }
