5947572ede7c84d449b97b4c63ef55d247f1613d
Amy While <26681721+elihwyma@users.noreply.github.com>
2022-12-09 19:28:28 +0000

    boopy bapp

--- Sileo/Backend/Dependency Resolver/DependencyResolverAccelerator.swift
+++ Sileo/Backend/Dependency Resolver/DependencyResolverAccelerator.swift
@@ -11,46 +11,21 @@ import Evander
 
 class DependencyResolverAccelerator {
     public static let shared = DependencyResolverAccelerator()
+    private var partialRepoList: [URL: Set<Package>] = [:]
+    private var packageList: Set<Package> = []
     private var preflightedRepos = false
-    
-    struct PreflightedPackage: Hashable {
-        let version: String
-        let bundleID: String
-        let data: Data
-        
-        init(package: Package) {
-            self.version = package.version
-            self.bundleID = package.packageID
-            self.data = package.rawData ?? Data()
-        }
-        
-        static func ==(lhs: PreflightedPackage, rhs: PreflightedPackage) -> Bool {
-            lhs.version == rhs.version && lhs.bundleID == rhs.bundleID
-        }
-        
-        static func ==(lhs: PreflightedPackage, rhs: Package) -> Bool {
-            lhs.bundleID == rhs.packageID && lhs.version == rhs.version
-        }
-        
-        func hash(into hasher: inout Hasher) {
-            hasher.combine(bundleID)
-            hasher.combine(version)
-        }
-    }
-    
-    private var preflightedPackages: [URL: Set<PreflightedPackage>] = [:]
-    private var toBePreflighted: [URL: Set<PreflightedPackage>] = [:]
     
     public func preflightInstalled() {
         if Thread.isMainThread {
             fatalError("Don't call things that will block the UI from the main thread")
         }
        
+        partialRepoList = [:]
         try? getDependencies(packages: Array(PackageListManager.shared.installedPackages.values))
         preflightedRepos = true
     }
     
-    private var depResolverPrefix: URL = {
+    private var depResolverPrefix: URL {
         #if targetEnvironment(simulator) || TARGET_SANDBOX
         let listsURL = FileManager.default.documentDirectory.appendingPathComponent("sileolists")
         if !listsURL.dirExists {
@@ -59,29 +34,6 @@ class DependencyResolverAccelerator {
         return listsURL
         #else
         return URL(fileURLWithPath: CommandPath.sileolists)
-        #endif
-    }()
-    
-    init() {
-        #if targetEnvironment(simulator) || TARGET_SANDBOX
-        try? FileManager.default.removeItem(atPath: CommandPath.sileolists)
-        #else
-        spawnAsRoot(args: [CommandPath.rm, "-rf", CommandPath.sileolists])
-        spawnAsRoot(args: [CommandPath.mkdir, "-p", CommandPath.sileolists])
-        spawnAsRoot(args: [CommandPath.chown, "-R", CommandPath.group, CommandPath.sileolists])
-        spawnAsRoot(args: [CommandPath.chmod, "-R", "0755", CommandPath.sileolists])
-        #endif
-    }
-    
-    public func removeRepo(repo: Repo) {
-        let url = RepoManager.shared.cacheFile(named: "Packages", for: repo)
-        let newSourcesFile = depResolverPrefix.appendingPathComponent(url.lastPathComponent)
-        toBePreflighted.removeValue(forKey: url)
-        preflightedPackages.removeValue(forKey: url)
-        #if targetEnvironment(simulator) || TARGET_SANDBOX
-        try? FileManager.default.removeItem(at: newSourcesFile)
-        #else
-        spawnAsRoot(args: [CommandPath.rm, "-rf", newSourcesFile.aptPath])
         #endif
     }
     
@@ -92,11 +44,18 @@ class DependencyResolverAccelerator {
         PackageListManager.shared.initWait()
 
         for package in packages {
-            getDependenciesInternal(package: package)
+            getDependenciesInternal2(package: package)
         }
         
+        #if targetEnvironment(simulator) || TARGET_SANDBOX
+        #else
+        spawnAsRoot(args: [CommandPath.rm, "-rf", CommandPath.sileolists])
+        spawnAsRoot(args: [CommandPath.mkdir, "-p", CommandPath.sileolists])
+        spawnAsRoot(args: [CommandPath.chown, "-R", CommandPath.group, CommandPath.sileolists])
+        spawnAsRoot(args: [CommandPath.chmod, "-R", "0755", CommandPath.sileolists])
+        #endif
         let resolverPrefix = depResolverPrefix
-        for (sourcesFile, packages) in toBePreflighted {
+        for (sourcesFile, packages) in partialRepoList {
             if sourcesFile.lastPathComponent == "status" || sourcesFile.scheme == "local" {
                 continue
             }
@@ -104,47 +63,44 @@ class DependencyResolverAccelerator {
             
             var sourcesData = Data()
             for package in packages {
-                var bytes = [UInt8](package.data)
-                if bytes.suffix(2) != [10, 10] { // \n\n
-                    if bytes.last == 10 {
-                        bytes.append(10)
+                guard let packageData = package.rawData else {
+                    continue
+                }
+                var string = String(decoding: packageData, as: UTF8.self)
+                if string.suffix(2) != "\n\n" {
+                    if string.last == "\n" {
+                        string += "\n"
                     } else {
-                        bytes.append(contentsOf: [10, 10])
+                        string += "\n\n"
                     }
                 }
-                sourcesData.append(Data(bytes))
+                guard let data = string.data(using: .utf8) else { continue }
+                sourcesData.append(data)
             }
             do {
-                try sourcesData.append(to: newSourcesFile.aptUrl)
+                try sourcesData.write(to: newSourcesFile.aptUrl)
             } catch {
                 throw error
             }
-            
-            toBePreflighted.removeValue(forKey: sourcesFile)
-            let preflighted = preflightedPackages[sourcesFile] ?? Set<PreflightedPackage>()
-            preflightedPackages[sourcesFile] = preflighted.union(packages)
         }
     }
     
     private func getDependenciesInternal(package: Package) {
-        let url = package.sourceFileURL?.aptUrl ?? URL(string: "local://")!
-        if preflightedPackages[url]?.contains(where: { $0 == package }) ?? false {
-            return
-        }
         for packageVersion in package.allVersions {
-            getDependenciesInternal2(package: packageVersion, sourceFileURL: url)
+            getDependenciesInternal2(package: packageVersion)
         }
     }
    
-    private func getDependenciesInternal2(package: Package, sourceFileURL: URL) {
-
-        if toBePreflighted[sourceFileURL]?.contains(where: { $0 == package }) ?? false {
-            return
-        } else if toBePreflighted[sourceFileURL] == nil {
-            toBePreflighted[sourceFileURL] = Set<PreflightedPackage>()
+    private func getDependenciesInternal2(package: Package) {
+        let sourceFileURL = package.sourceFileURL?.aptUrl ?? URL(string: "local://")!
+        if partialRepoList[sourceFileURL] == nil {
+            partialRepoList[sourceFileURL] = Set<Package>()
         }
-        toBePreflighted[sourceFileURL]?.insert(PreflightedPackage(package: package))
-  
+        if partialRepoList[sourceFileURL]?.contains(package) ?? false {
+            return
+        }
+        partialRepoList[sourceFileURL]?.insert(package)
+        
         // Depends, Pre-Depends, Recommends, Suggests, Breaks, Conflicts, Provides, Replaces, Enhance
         let packageKeys = ["depends", "pre-depends", "conflicts", "replaces", "recommends", "provides", "breaks"]
         
@@ -180,4 +136,5 @@ class DependencyResolverAccelerator {
         }
         return packageIds
     }
+    
 }
--- Sileo/Backend/Repo Manager/RepoManager.swift
+++ Sileo/Backend/Repo Manager/RepoManager.swift
@@ -247,7 +247,6 @@ final class RepoManager {
         for repo in repos {
             DatabaseManager.shared.deleteRepo(repo: repo)
             PaymentManager.shared.removeProviders(for: repo)
-            DependencyResolverAccelerator.shared.removeRepo(repo: repo)
         }
         NotificationCenter.default.post(name: NewsViewController.reloadNotification, object: nil)
     }
