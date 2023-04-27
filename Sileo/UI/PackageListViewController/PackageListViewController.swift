fb2f6a4a16d7e714fb791276bf92b1380ef7e3f9
Foxfort Mobile <45145818+foxfortmobile@users.noreply.github.com>
2023-04-27 22:35:02 +0400

    Add package identifier in export list (#286)

--- Sileo/UI/PackageListViewController/PackageListViewController.swift
+++ Sileo/UI/PackageListViewController/PackageListViewController.swift
@@ -351,7 +351,7 @@ class PackageListViewController: SileoViewController, UIGestureRecognizerDelegat
             }
             let packageVersion = package.version
             
-            bodyFromArray += "\(packageName): \(packageVersion)\n"
+            bodyFromArray += "\(packageName):(\(package.packageID)) \(packageVersion)\n"
         }
         
         if let subRange = Range<String.Index>(NSRange(location: bodyFromArray.count - 1, length: 1), in: bodyFromArray) {
